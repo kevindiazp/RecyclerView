91b792399ddf670af9974018e95bfe6d05e85541
ucupalandi12 <ucupers1297@gmail.com>
2018-03-01 18:57:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/admin/recyclerview/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.admin.recyclerview;
+
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    int mCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + mCount++);
+            Log.d("WordList", mWordList.getLast());
+        }
+        // Get a handle to the RecyclerView.
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        // Create an adapter and supply the data to be displayed.
+        mAdapter = new WordListAdapter(this, mWordList);
+        // Connect the adapter with the RecyclerView.
+        mRecyclerView.setAdapter(mAdapter);
+        // Give the RecyclerView a default layout manager.
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                int wordListSize = mWordList.size();
+
+                mWordList.addLast("+ word" + wordListSize);
+
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+
+                mRecyclerView.smoothScrollToPosition(wordListSize);
+
+            }
+        });
+
+    }
+
+
+}
